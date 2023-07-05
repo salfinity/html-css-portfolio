01839dbeefee3c28cf785dfb52f645d164ed5dcb
mohsalim <moharts2000@gmail.com>
2023-07-06 00:23:09 +0300

    update js color

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+		header.style.backgroundColor = '#0d0d0d';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
